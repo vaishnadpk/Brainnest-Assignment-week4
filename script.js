15c62b02e85bb3d1e67115ead855bd5e99d78ad4
vaishnadpk <107250778+vaishnadpk@users.noreply.github.com>
2022-10-22 00:14:20 +0200

    final commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let previousValue = 0;
 let operator = "";
 
+//function to set the operator
 function setOperator(opValue) {
   operator = opValue;
   previousValue = getDisplayValue();
@@ -8,35 +9,39 @@ function setOperator(opValue) {
   document.querySelector("#display").placeholder = previousValue;
 }
 
+//function for appending the values
 function setDisplay(value) {
   let disValue = getDisplayValue();
-  if (disValue === 0) {
+  if (disValue === "0") {
     disValue = "";
   }
-  if (value === "." && disValue.indexOf(".") > -1) {
+  if (value === "." && disValue.includes(".")) {
     return;
   }
 
   setDisplayValue(`${disValue}${value}`);
 }
 
+//function for the calculation
 function calculate() {
   let value = getDisplayValue();
   let result = 0;
+  let firstNumber = Number.parseFloat(previousValue);
+  let secondNumber = Number.parseFloat(value);
   switch (operator) {
     case "+":
-      result = Number.parseFloat(previousValue) + Number.parseFloat(value);
+      result = firstNumber + secondNumber;
       break;
     case "-":
-      result = Number.parseFloat(previousValue) - Number.parseFloat(value);
+      result = firstNumber - secondNumber;
 
       break;
     case "*":
-      result = Number.parseFloat(previousValue) * Number.parseFloat(value);
+      result = firstNumber * secondNumber;
 
       break;
     case "/":
-      result = Number.parseFloat(previousValue) / Number.parseFloat(value);
+      result = firstNumber / secondNumber;
 
       break;
     default:
@@ -46,11 +51,13 @@ function calculate() {
   setDisplayValue(result);
 }
 
+//function to get the value of input
 function getDisplayValue() {
   let displayValue = document.querySelector("#display").value;
   return displayValue;
 }
 
+//function to set the value to value attribute of input
 function setDisplayValue(value) {
   document.querySelector("#display").value = value;
 }
